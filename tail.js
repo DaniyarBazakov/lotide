974add26a452506a138a7527a45032192aac7da8
Daniyar Bazakov <bazakovd@gmail.com>
2024-05-17 17:56:53 -0400

    added a check for empty arrays

--- tail.js
+++ tail.js
@@ -16,12 +16,13 @@ assertEqual(result.length, 2);
 assertEqual(result[0], "Lighthouse");
 assertEqual(result[1], "Labs");
 
-const emptyarray = tail(["Hello"]);
-assertEqual(emptyarray.length, 0);
+const emptyArray = tail(["Hello"]);
+assertEqual(emptyArray.length, 0);
 
 const words = ["Yo Yo", "Lighthouse", "Labs"];
 tail(words);
 assertEqual(words.length, 3);
 
-
+const emptyInput = tail([]);
+assertEqual(emptyInput.length, 0);
 
