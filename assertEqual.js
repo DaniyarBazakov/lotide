20e2899a7cb2d80c2878d030e9fdf532e7716b8d
Daniyar Bazakov <bazakovd@gmail.com>
2024-05-17 16:36:12 -0400

    added assertEqual.js

---
+++ assertEqual.js
@@ -0,0 +1,13 @@
+// FUNCTION IMPLEMENTATION
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log("\u{1F64C} Assertion Passed: " + actual + " === " + expected);
+  } else {
+    console.log("\u{1F62A} Assertion Failed: " + actual + " !== " + expected);
+  }
+};
+
+// TEST CODE
+assertEqual("Lighthouse Labs", "Bootcamp");
+assertEqual(1, 1);
+
