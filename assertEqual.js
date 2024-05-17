e8f3aa86783b77fe3ea2b7321b0acb542ea02cd2
Daniyar Bazakov <bazakovd@gmail.com>
2024-05-17 17:08:12 -0400

    added fancier stings

--- assertEqual.js
+++ assertEqual.js
@@ -1,9 +1,9 @@
 // FUNCTION IMPLEMENTATION
 const assertEqual = function(actual, expected) {
   if (actual === expected) {
-    console.log("\u{1F64C} Assertion Passed: " + actual + " === " + expected);
+    console.log(`\u{1F64C} Assertion Passed: ${actual} === ${expected}`);
   } else {
-    console.log("\u{1F62A} Assertion Failed: " + actual + " !== " + expected);
+    console.log(`\u{1F62A} Assertion Failed: ${actual} !== ${expected}`);
   }
 };
 
