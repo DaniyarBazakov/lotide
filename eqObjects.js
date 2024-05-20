d5a9b75f0b434f4a4810aa42967571f5abf2997e
Daniyar Bazakov <bazakovd@gmail.com>
2024-05-20 13:20:36 -0400

    pushing eqObjects.js

---
+++ eqObjects.js
@@ -0,0 +1,34 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`\u{1F64C} Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`\u{1F62A} Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+
+const eqObjects = function(object1, object2) {
+  //Object.keys(shirtObject); // => returns ["color", "size"]
+  const keys1 = Object.keys(object1);
+  const keys2 = Object.keys(object2);
+
+  if (keys1.length !== keys2.length) {
+    return false;
+  }
+  for (let key of keys1) {
+    if (object1[key] !== object2[key]) {
+      return false;
+    }
+  }
+  return true;
+};
+
+
+const shirtObject = { color: "red", size: "medium" };
+const anotherShirtObject = { size: "medium", color: "red" };
+eqObjects(shirtObject, anotherShirtObject); // => true
+assertEqual(eqObjects(shirtObject, anotherShirtObject), true);
+
+const longSleeveShirtObject = { size: "medium", color: "red", sleeveLength: "long" };
+eqObjects(shirtObject, longSleeveShirtObject); // => false
+assertEqual(eqObjects(shirtObject, longSleeveShirtObject), false);
