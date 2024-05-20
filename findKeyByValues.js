813177e5fba02fd72b541dc6606c000a82bbf189
Daniyar Bazakov <bazakovd@gmail.com>
2024-05-20 12:59:10 -0400

    pushing findKeyByValues.js

---
+++ findKeyByValues.js
@@ -0,0 +1,27 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`\u{1F64C} Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`\u{1F62A} Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+
+const findKeyByValue = function(object, str) {
+  for (let obj in object) {
+    if (object[obj] === str) {
+      return obj;
+    }
+  }
+  return undefined;
+};
+
+
+const bestTVShowsByGenre = {
+  sci_fi: "The Expanse",
+  comedy: "Brooklyn Nine-Nine",
+  drama: "The Wire",
+};
+
+assertEqual(findKeyByValue(bestTVShowsByGenre, "The Wire"), "drama");
+assertEqual(findKeyByValue(bestTVShowsByGenre, "That '70s Show"), undefined);
