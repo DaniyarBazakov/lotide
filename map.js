06183d463493a98282f046f1155f9e25d27ff0bd
Daniyar Bazakov <bazakovd@gmail.com>
2024-05-22 17:22:48 -0400

    pushing map.js

---
+++ map.js
@@ -0,0 +1,40 @@
+const words = ["ground", "control", "to", "major", "tom"];
+
+const map = function(array, callback) {
+  const results = [];
+  for (let item of array) {
+    results.push(callback(item));
+  }
+  return results;
+};
+
+const results1 = map(words, (word) => word[0]);
+
+console.log(results1);
+
+
+const eqArrays = function(arr1, arr2) {
+  if (arr1.length !== arr2.length) {
+    return false;
+  }
+  for (let i = 0; i < arr1.length; i++) {
+    if (arr1[i] !== arr2[i]) {
+      return false;
+    }
+  }
+  return true;
+};
+
+const assertArraysEqual = function(arr1, arr2) {
+  let equal = eqArrays(arr1, arr2);
+  if (equal === false) {
+    console.log(`${arr1} is not equal to ${arr2}`);
+  } else {
+    console.log(`${arr1} is equal to ${arr2}`);
+  }
+  
+};
+
+assertArraysEqual(map(["hello", "bye"], (word) => word[0]), ["h", "b"]);
+assertArraysEqual(map([], (word) => word[0]), []);
+assertArraysEqual(map(["hello", "ciao", "Required"], (word) => word[0]), ["h", "c", "R"]);
