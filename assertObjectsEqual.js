7055de3d84b6e4dee4e5586355b6514ea7da3f27
Daniyar Bazakov <bazakovd@gmail.com>
2024-06-07 12:05:31 -0400

    refactor assertObjectEqual.js

--- assertObjectsEqual.js
+++ assertObjectsEqual.js
@@ -1,34 +1,4 @@
-const eqArrays = function(arr1, arr2) {
-  if (arr1.length !== arr2.length) {
-    return false;
-  }
-  for (let i = 0; i < arr1.length; i++) {
-    if (arr1[i] !== arr2[i]) {
-      return false;
-    }
-  }
-  return true;
-};
-
-const eqObjects = function(object1, object2) {
-  //Object.keys(shirtObject); // => returns ["color", "size"]
-  const keys1 = Object.keys(object1);
-  const keys2 = Object.keys(object2);
-
-  if (keys1.length !== keys2.length) {
-    return false;
-  }
-  for (let key of keys1) {
-    if (Array.isArray(object1[key]) && Array.isArray(object2[key])) {
-      if (!eqArrays(object1[key], object2[key])) {
-        return false;
-      }
-    } else if (object1[key] !== object2[key]) {
-      return false;
-    }
-  }
-  return true;
-};
+const eqObjects = require("./eqObjects");
 
 const assertObjectsEqual = function(actual, expected) {
   const inspect = require("util").inspect;
@@ -39,10 +9,5 @@ const assertObjectsEqual = function(actual, expected) {
     console.log(`\u{1F62A} Assertion Failed: ${inspect(actual)} !== ${inspect(expected)}`);
   }
 };
-
-
-assertObjectsEqual({h: 1}, {h: 1});
-assertObjectsEqual({h: 1}, {h: 2});
-
 
 module.exports = assertObjectsEqual;
