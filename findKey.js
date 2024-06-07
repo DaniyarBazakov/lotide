0cdaa1001adbd41d1d11bbcfa2b20c9e7497aad6
Daniyar Bazakov <bazakovd@gmail.com>
2024-06-07 12:18:04 -0400

    refactor findKey.js, add test for findKeyTest.js

--- findKey.js
+++ findKey.js
@@ -1,12 +1,3 @@
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`\u{1F64C} Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`\u{1F62A} Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
-
-
 const findKey = function(object, callback) {
   for (let obj in object) {
     if (callback(object[obj])) {
@@ -16,28 +7,4 @@ const findKey = function(object, callback) {
   return undefined;
 };
 
-const result = findKey(
-  {
-    "Blue Hill": { stars: 1 },
-    Akaleri: { stars: 3 },
-    noma: { stars: 2 },
-    elBulli: { stars: 3 },
-    Ora: { stars: 2 },
-    Akelarre: { stars: 3 },
-  },
-  (x) => x.stars === 2
-);
-
-const result1 = findKey(
-  {
-    "Toyota": { engine: 1.8 },
-    BMW: { engine: 3.0 },
-    Mercedes: { engine: 5.5 },
-    Mazde: { engine: 2.5 },
-  },
-  (x) => x.engine === 5.5
-);
-
-
-assertEqual(result, "noma");
-assertEqual(result1, "Mercedes");
+module.exports = findKey;
---
+++ test/findKeyTest.js
@@ -0,0 +1,33 @@
+const assert = require('chai').assert;
+const findKey = require("../findKey");
+
+const result = findKey(
+  {
+    "Blue Hill": { stars: 1 },
+    Akaleri: { stars: 3 },
+    noma: { stars: 2 },
+    elBulli: { stars: 3 },
+    Ora: { stars: 2 },
+    Akelarre: { stars: 3 },
+  },
+  (x) => x.stars === 2
+);
+
+const result1 = findKey(
+  {
+    "Toyota": { engine: 1.8 },
+    BMW: { engine: 3.0 },
+    Mercedes: { engine: 5.5 },
+    Mazde: { engine: 2.5 },
+  },
+  (x) => x.engine === 5.5
+);
+
+describe("#findKey", () => {
+  it("returns 'noma'", () => {
+    assert.strictEqual(result, "noma");
+  });
+  it("returns 'Mercedes'", () => {
+    assert.strictEqual(result1, "Mercedes"); 
+  });
+});
