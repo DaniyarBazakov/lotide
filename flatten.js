28d89d20f889ceb159bf7f1e35ce5d15ccf8e4f1
Daniyar Bazakov <bazakovd@gmail.com>
2024-06-07 09:38:29 -0400

    refactor flatten.js, add flattenTest.js

--- flatten.js
+++ flatten.js
@@ -1,42 +1,3 @@
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`\u{1F64C} Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`\u{1F62A} Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
-
-const assertArraysEqual = function(arr1, arr2) {
-  let equal = true;
-  if (arr1.length !== arr2.length) {
-    equal = false;
-  }
-  for (let i = 0; i < arr1.length; i++) {
-    if (arr1[i] !== arr2[i]) {
-      equal = false;
-    }
-  }
-  if (equal === false) {
-    console.log(`${arr1} is not equal to ${arr2}`);
-  } else {
-    console.log(`${arr1} is equal to ${arr2}`);
-  }
-  
-};
-
-const eqArrays = function(arr1, arr2) {
-  if (arr1.length !== arr2.length) {
-    return false;
-  }
-  for (let i = 0; i < arr1.length; i++) {
-    if (arr1[i] !== arr2[i]) {
-      return false;
-    }
-  }
-
-  return true;
-};
-
 const flatten = function(arr) {
   let flattenedArray = [];
 
@@ -51,9 +12,4 @@ const flatten = function(arr) {
   return flattenedArray;
 };
 
-assertArraysEqual(flatten([1, 2, [3, 4], 5, [6]]), [1, 2, 3, 4, 5, 6]);
-assertArraysEqual(flatten([1, [2, 3], [4]]), [1, 2, 3, 4]);
-assertEqual(eqArrays(flatten([1, 2, [3, 4], 5, [6]]), [1, 2, 3, 4, 5, 6]), true);
-assertEqual(eqArrays(flatten([[1, 2, 3], [4, 5], [6, 7, 8]]), [1, 2, 3, 5, 6, 7, 8]), false);
-
-
+module.exports = flatten;
